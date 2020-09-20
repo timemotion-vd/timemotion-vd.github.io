d3145d27322e503edeeda8048136dfff5d904594
timemotion-vd <timemotion.vd@gmail.com>
2020-09-20 23:19:10 +0800

    20200920

--- c/captcha/x5captcha.php
+++ c/captcha/x5captcha.php
@@ -1,7 +1,7 @@
 <?php
 include("../res/x5engine.php");
-$nameList = array("mrc","gsh","y5n","yhj","6we","jkv","hc8","cyd","j6l","nz3");
-$charList = array("H","C","F","P","7","8","F","K","5","5");
+$nameList = array("hyz","uwt","ptv","y3f","s87","86u","85e","6p4","nuw","n24");
+$charList = array("C","6","K","F","U","C","M","U","N","N");
 $cpt = new X5Captcha($nameList, $charList);
 //Check Captcha
 if ($_GET["action"] == "check")
